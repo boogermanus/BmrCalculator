03e1e5aa20774039383bc1ab78c9dde453f69521
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-02 20:57:59 -0500

    basic layout for bmr component

--- ClientApp/src/app/material.module.ts
+++ ClientApp/src/app/material.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
     declarations: [],
-    imports: [MatSelectModule],
-    exports: [MatSelectModule]
+    imports: [
+        MatSelectModule,
+        MatInputModule,
+        MatFormFieldModule,
+    ],
+    exports: [
+        MatSelectModule,
+        MatInputModule,
+        MatFormFieldModule,
+    ]
 })
 export class MaterialModule { }
