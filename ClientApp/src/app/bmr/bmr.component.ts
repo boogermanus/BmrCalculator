47252b7c6e686576260f0f5787c46558620851f6
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-02 21:35:59 -0500

    wire form and controls

--- ClientApp/src/app/bmr/bmr.component.ts
+++ ClientApp/src/app/bmr/bmr.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GenderConstants } from '../models/gender-constants';
 import { UnitOfMassConstants } from '../models/units-of-mass-constants';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bmr',
@@ -11,9 +12,38 @@ export class BmrComponent implements OnInit {
 
   genders: any[] = GenderConstants.getGenders();
   unitsOfMass: any[] = UnitOfMassConstants.getUnitsOfMass();
-  constructor() { }
+  gender: FormControl;
+  unitOfMass: FormControl;
+  name: FormControl;
+  age: FormControl;
+  weight: FormControl;
+  form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.form = this.buildForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  private buildForm(): FormGroup {
+    this.gender = new FormControl('', Validators.required);
+    this.unitOfMass = new FormControl('', Validators.required);
+    this.name = new FormControl('',
+      Validators.compose([Validators.required, Validators.maxLength(100)]));
+    this.age = new FormControl(0,
+      Validators.compose([Validators.required, Validators.min(0)]));
+    this.weight = new FormControl(0,
+        Validators.compose([Validators.required, Validators.min(0)]));
+
+    return this.formBuilder.group({
+      gender: this.gender,
+      unitOfMass: this.unitOfMass,
+      name: this.name,
+      age: this.age,
+      weight: this.weight,
+    });
   }
 
 }
