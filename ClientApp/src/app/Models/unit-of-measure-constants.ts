5cd15320af705c22bcc8f042e8b6349ea84624d2
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-09 23:43:02 -0500

    minor formatting

--- ClientApp/src/app/Models/unit-of-measure-constants.ts
+++ ClientApp/src/app/Models/unit-of-measure-constants.ts
@@ -3,8 +3,8 @@ export class UnitOfMeasureConstants {
     public static readonly METRIC = 'm';
 
     private static unitsOfMeasure: any[] = [
-        {typeValue: UnitOfMeasureConstants.IMPERIAL, caption: 'Imperial'},
-        {typeValue: UnitOfMeasureConstants.METRIC, caption: 'Metric'}
+        { typeValue: UnitOfMeasureConstants.IMPERIAL, caption: 'Imperial' },
+        { typeValue: UnitOfMeasureConstants.METRIC, caption: 'Metric' }
     ];
 
     public static getUnitsOfMeasure(): any[] {
