b5abb0ac402d98484d9efd9b68fd4211a65ae48d
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-09 22:21:19 -0500

    bmr service

---
+++ ClientApp/src/app/services/bmr.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IBmr } from '../models/ibmr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BmrService {
+
+  private URL: string;
+  readonly CONTROLLER = 'bmr';
+  constructor(private httpClient: HttpClient,
+    @Inject('BASE_URL') private baseURL: string) {
+    this.URL = `${this.baseURL}${this.CONTROLLER}`;
+  }
+
+  saveBmr(bmr: IBmr): Promise<IBmr> {
+    return this.httpClient.post<IBmr>(this.URL, bmr).toPromise();
+  }
+
+  getBmrs(): Promise<IBmr[]> {
+    return this.httpClient.get<IBmr[]>(this.URL).toPromise();
+  }
+}
