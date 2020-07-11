d110ab33de9c8b008b8388f0b0c2c09d5ad15221
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-10 19:50:41 -0500

    further formatting

--- ClientApp/src/app/bmr/bmr.component.ts
+++ ClientApp/src/app/bmr/bmr.component.ts
@@ -4,7 +4,7 @@ import { IBmr } from '../models/ibmr';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
-import { DatePipe } from '@angular/common';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-bmr',
@@ -15,27 +15,32 @@ export class BmrComponent implements OnInit {
 
   // we need a date pipe in code so that we can transform the created date
   readonly datePipe: DatePipe = new DatePipe('en-US');
+  readonly decimalPipe: DecimalPipe = new DecimalPipe('en-US');
 
   // column definiations
   columns = [
-    {columnDef: 'age', header: 'Age'},
-    {columnDef: 'weight', header: 'Weight'},
-    {columnDef: 'height', header: 'Height'},
-    {columnDef: 'bmr', header: 'BMR'},
+    { columnDef: 'age', header: 'Age' },
+    {
+      columnDef: 'weight',
+      header: 'Weight',
+      cell: (element: any) => `${this.decimalPipe.transform(element.weight, '1.2-2')}`},
+    { columnDef: 'height', header: 'Height' },
+    { columnDef: 'bmr', header: 'BMR' },
     {
       columnDef: 'createdOn',
       header: 'Created On',
       // notice that we can provide a cell property to privide the cell value if we need to do a transform
-      cell: (element: any) => `${this.datePipe.transform(element.createdOn, 'short')}`}
+      cell: (element: any) => `${this.datePipe.transform(element.createdOn, 'short')}`
+    }
   ];
 
   // still need this, afraid to try something else...
   displayedColumns = this.columns.map(c => c.columnDef);
   dataSource: MatTableDataSource<IBmr>;
-  @ViewChild(MatSort, {static: true})sort: MatSort;
-  @ViewChild(MatPaginator, {static: true})paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private bmrService: BmrService) {}
+  constructor(private bmrService: BmrService) { }
 
   async ngOnInit(): Promise<void> {
     this.dataSource = new MatTableDataSource<IBmr>(await this.bmrService.getBmrs());
