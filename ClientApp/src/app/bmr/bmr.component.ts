925dcbb7ebc0b6b0a3e16e46658d5814976a2dd6
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-10 19:04:03 -0500

    basic mat table with generic columns

--- ClientApp/src/app/bmr/bmr.component.ts
+++ ClientApp/src/app/bmr/bmr.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BmrService } from '../services/bmr.service';
 import { IBmr } from '../models/ibmr';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-bmr',
@@ -9,12 +12,25 @@ import { IBmr } from '../models/ibmr';
 })
 export class BmrComponent implements OnInit {
 
-  data: IBmr[];
+  columns = [
+    {columnDef: 'age', header: 'Age'},
+    {columnDef: 'weight', header: 'Weight'},
+    {columnDef: 'height', header: 'Height'},
+    {columnDef: 'bmr', header: 'BMR'},
+    {columnDef: 'createdOn', header: 'Created On'}
+  ];
+
+  displayedColumns = this.columns.map(c => c.columnDef);
+  dataSource: MatTableDataSource<IBmr>;
+  @ViewChild(MatSort, {static: true})sort: MatSort;
+  @ViewChild(MatPaginator, {static: true})paginator: MatPaginator;
 
   constructor(private bmrService: BmrService) {}
 
   async ngOnInit(): Promise<void> {
-    this.data = await this.bmrService.getBmrs();
+    this.dataSource = new MatTableDataSource<IBmr>(await this.bmrService.getBmrs());
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
--- ClientApp/src/app/material.module.ts
+++ ClientApp/src/app/material.module.ts
@@ -3,6 +3,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 @NgModule({
     declarations: [],
     imports: [
@@ -10,12 +14,18 @@ import { MatButtonModule } from '@angular/material/button';
         MatInputModule,
         MatFormFieldModule,
         MatButtonModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
     ],
     exports: [
         MatSelectModule,
         MatInputModule,
         MatFormFieldModule,
         MatButtonModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
     ]
 })
 export class MaterialModule { }
