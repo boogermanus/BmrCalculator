89b9fc4c6c91fd7335056609c8b27a21831ce91b
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-15 18:25:31 -0500

    drop bms due to decimal issue

---
+++ Data/Migrations/20200715232319_DropBrmsBecauseOfDecimal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BmrCalculator.Data.Migrations
+{
+    public partial class DropBrmsBecauseOfDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BMRs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BMRs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    age = table.Column<int>(type: "INTEGER", nullable: false),
+                    bmr = table.Column<decimal>(type: "TEXT", nullable: false),
+                    createdOn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    height = table.Column<decimal>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    userId = table.Column<string>(type: "TEXT", nullable: true),
+                    weight = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BMRs", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_BMRs_AspNetUsers_userId",
+                        column: x => x.userId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BMRs_userId",
+                table: "BMRs",
+                column: "userId");
+        }
+    }
+}
