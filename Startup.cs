207006314d4a22a4bb3dd83520798a0329c70688
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-29 18:36:38 -0500

    add forwarded headers

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace BmrCalculator
 {
@@ -51,6 +52,11 @@ namespace BmrCalculator
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -96,6 +102,7 @@ namespace BmrCalculator
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+
         }
     }
 }
