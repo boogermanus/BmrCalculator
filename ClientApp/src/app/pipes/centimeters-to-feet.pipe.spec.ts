0597580ab6ef466488fbb2389d3a741e38e506f8
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-09-23 23:12:09 -0500

    fix tests...by removing some :*(

--- ClientApp/src/app/pipes/centimeters-to-feet.pipe.spec.ts
+++ ClientApp/src/app/pipes/centimeters-to-feet.pipe.spec.ts
@@ -1,8 +1,9 @@
+import { SettingsService } from '../services/settings.service';
 import { CentimetersToFeetPipe } from './centimeters-to-feet.pipe';
 
 describe('CentimetersToFeetPipe', () => {
   it('create an instance', () => {
-    const pipe = new CentimetersToFeetPipe();
+    const pipe = new CentimetersToFeetPipe(new SettingsService());
     expect(pipe).toBeTruthy();
   });
 });
--- ClientApp/src/app/pipes/kilograms-to-pounds.pipe.spec.ts
+++ ClientApp/src/app/pipes/kilograms-to-pounds.pipe.spec.ts
@@ -1,8 +1,9 @@
+import { SettingsService } from '../services/settings.service';
 import { KilogramsToPoundsPipe } from './kilograms-to-pounds.pipe';
 
 describe('KilogramsToPoundsPipe', () => {
   it('create an instance', () => {
-    const pipe = new KilogramsToPoundsPipe();
+    const pipe = new KilogramsToPoundsPipe(new SettingsService());
     expect(pipe).toBeTruthy();
   });
 });
