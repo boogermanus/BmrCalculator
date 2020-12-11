9255ca38bc4152397b7fe82b5bdc4b8e14d4c4c2
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-12-10 20:41:14 -0600

    add identity scaffold

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using BmrCalculator.Data;
+using BmrCalculator.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BmrCalculator.Areas.Identity.IdentityHostingStartup))]
+namespace BmrCalculator.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
