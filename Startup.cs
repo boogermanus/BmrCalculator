5c4e30894f2205fb09f34bf4368e07c997a340b2
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-12-28 19:41:07 -0600

    remove email confirmation

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace BmrCalculator
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
